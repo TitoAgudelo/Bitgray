15326c30b673e16aafeb7fb71c4b6aecd6ff6d93
TitoAgudelo <titoarturoagudelo@gmail.com>
2015-08-28 10:09:59 -0500

    list abilities and create one ramdom

--- _res/javascripts/angular/src/controllers.js
+++ _res/javascripts/angular/src/controllers.js
@@ -7,6 +7,13 @@ ctrls.controller('AbilitiesCtrl', ['$scope', '$http',
     $http.get('stubs/abilities.json').success(function(data) {
     	$scope.abilities = data;
 	});
+
+	$scope.addAbility =  function () {
+		$http.get('stubs/newAbilities.json').success(function(data) {
+    	var item = data[Math.floor(Math.random()*data.length)];
+    	$scope.abilities.push(item);
+	});
+	};
 }]);
 
 // ctrls.controller('ShotListCtrl', function ($scope, PagedResult, $routeParams){
