0ed4a6a01429e2312c5eaeb99ce0730f93140477
TitoAgudelo <titoarturoagudelo@gmail.com>
2015-08-28 18:19:16 -0500

    validation for form

--- _res/javascripts/angular/src/controllers.js
+++ _res/javascripts/angular/src/controllers.js
@@ -20,11 +20,51 @@ ctrls.controller('FormCtrl', ['$scope', '$http',
   function ($scope, $http) {
 
   	$scope.validate = function() {
+  		var user = $scope.user;
+  		var pass = $scope.password;
+  		if(user || pass) {
+
+			if (user) {
+				$scope.validateUser(user);
+			} 
+			if (pass) {
+				$scope.validatePassword(pass);
+			}
+			if(user && pass) {
+				$scope.validateUserPass(user, pass);
+			}
+
+		} else {
+			$scope.username = true;
+			$scope.pass = true;
+		}
 
   	};
-}]);
 
-ctrls.controller('ClockCtrl', ['$scope', '$http',
-  function ($scope, $http) {
+  	$scope.validateUser =  function (user) {
+		if (user.length < 5) {
+			$scope.username = true;
+		} else {
+			$scope.username = false;
+		}
+	}
 
+	$scope.validatePassword = function(pass) {
+		if (pass.length < 1 || pass.match(/\d+/g) == null) {
+			$scope.pass = true;
+		} else {
+			$scope.pass = false;
+		}
+	}
+
+	$scope.validateUserPass =  function(user, pass) {
+		if (user.length >= 5 && pass.length > 1 && pass.match(/\d+/g) != null) {
+			$scope.username = false;
+			$scope.pass = false;
+			window.location.href = '/';
+		} else {
+			$scope.validateUser(user);
+			$scope.validatePassword(pass);
+		}
+	}
 }]);
